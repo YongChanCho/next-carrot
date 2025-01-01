12e5a2704c9c3e3f96d51ff309450185f5a8aa27
jyc0328 <sykc2002@gmail.com>
2025-01-01 21:43:29 +0900

    style:home 스타일 수정정

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,29 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-    <div className="flex flex-col items-center justify-between min-h-screen p-6">
-      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <span className="text-9xl">🥕</span>
-        <h1 className="text-4xl ">당근</h1>
-        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
-      </div>
-      <div className="flex flex-col items-center gap-3 w-full">
-        <Link
-          href="/create-account"
-          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
-        >
-          시작하기
-        </Link>
-        <div className="flex gap-2">
-          <span>이미 계정이 있나요?</span>
-          <Link href="/login" className="hover:underline">
-            로그인
-          </Link>
+      <div className="flex flex-col items-center justify-between min-h-screen p-6">
+        <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+          <span className="text-9xl">🥕</span>
+          <h1 className="text-4xl ">당근</h1>
+          <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
         </div>
-      </div>
-    </div>
+        <div className="flex flex-col items-center gap-3 w-full">
+          <Link
+            href="/create-account"
+            className="primary-btn py-2.5 text-lg"
+          >
+            시작하기
+          </Link>
+          <div className="flex gap-2">
+            <span>이미 계정이 있나요?</span>
+            <Link href="/login" className="hover:underline">
+              로그인
+            </Link>
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }
